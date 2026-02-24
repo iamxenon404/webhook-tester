4aa0f74552e0dfd60e5a28a8c6f24582b51c77fe
xenon <demiladeafuape74@gmail.com>
2026-02-25 00:09:44 +0100

    updated server

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import app from './app';
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Webhook Tester running on http://localhost:${PORT}`);
+  console.log(`POST http://localhost:${PORT}/create to generate a new webhook endpoint`);
+});
