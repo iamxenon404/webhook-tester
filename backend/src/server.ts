d56e3dafdea3277a5cacacf30be00080c3cfbd94
xenon <demiladeafuape74@gmail.com>
2026-02-26 19:04:14 +0100

    update backend server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
-import app from './app';
 import dotenv from 'dotenv';
+dotenv.config();
+
+import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Webhook Tester running on http://localhost:${PORT}`);
+  console.log(`🪝 Xenlog404 running on http://localhost:${PORT}`);
   console.log(`POST http://localhost:${PORT}/create to generate a new webhook endpoint`);
 });
